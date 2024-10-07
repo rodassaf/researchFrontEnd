0f573e04c36867baa94056292cd4d3e6657f4ce8
rodassaf <rodassaf@gmail.com>
2024-10-08 00:51:22 +0100

    morph targets

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//import init from "three/examples/jsm/offscreen/scene.js";
+import {Pane} from 'tweakpane';
+
 
 var userName = prompt( "Please enter your name" );
 
@@ -23,7 +24,11 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls( camera, renderer.domElement );
-
+// Create a UI Pane
+const pane = new Pane({
+    title: 'XR Collaboration',
+    expanded: true,
+  });
 // Instantiate a loader
 const loader = new GLTFLoader();
 
@@ -58,7 +63,8 @@ function init() {
             gltf.cameras; // Array<THREE.Camera>
             gltf.asset; // Object
             
-            fitCameraToObject(camera, gltf.scene, 1.6, controls);
+            fitCameraToObject( camera, gltf.scene, 1.6, controls );
+            createGUI( gltf.scene, gltf.animations );
         },
         // called while loading is progressing
         function ( xhr ) {
@@ -102,7 +108,7 @@ function fitCameraToObject(camera, object, offset, controls) {
     cameraDistance *= offset;
 
     // Set the camera position directly in front of the object
-    // Assuming 'front' means along the z-axis (you can modify this axis if needed)
+    // Assuming 'front' means along the z-axis
     const frontDirection = new THREE.Vector3(0, 0, 1); // Camera positioned along positive Z axis
     const cameraPosition = center.clone().add(frontDirection.multiplyScalar(cameraDistance));
     
@@ -121,7 +127,10 @@ function fitCameraToObject(camera, object, offset, controls) {
     camera.updateProjectionMatrix();
 }
 
-
+// Function to find a blade by label
+function findBladeByLabel(pane, label) {
+    return pane.children.find((child) => child.controller.value.value_ === label);
+}
 
 function noXRCameraUpdate () {
     socket.emit( 'updateCamera', { 
@@ -146,9 +155,9 @@ function render() {
   renderer.render( scene, camera );
 }
 
-init();
 
-animate();
+
+// Windows Behaviour *****************************************
 
 window.addEventListener( 'resize', onWindowResize );
 
@@ -158,6 +167,97 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+
+// GUI *******************************************************
+
+function createGUI( model, animations) {
+    // Objects with morphs
+    let objectsWithMorphTargets = [];
+    // Create object to bind with UI Options
+    let objectOptions = {
+        none: 'none',
+    };
+    let morphNameTargets = [];
+    // Create object to bind with UI and be the Final VALUE
+    let currentObjectSelection = {
+        morphObject: 'none',
+    };
+
+    // Find objects with Morphs or Blendshapes
+    model.traverseVisible( ( object ) => {
+            if ( object.isMesh && object.geometry.morphAttributes ) {
+                // Check if the object has morph targets
+                const hasMorphTargets = object.geometry.morphAttributes.position || object.geometry.morphAttributes.normal;
+                
+                if ( hasMorphTargets ) {
+                    // Push this object into array to be used on pane
+                    objectsWithMorphTargets.push( object.name );
+                }
+            }
+        }
+    );
+
+    // Feed the binded object for options
+    for(  let i = 0; i < objectsWithMorphTargets.length; i++  ){
+        objectOptions[objectsWithMorphTargets[i]] = objectsWithMorphTargets[i];
+    }
+  
+    if( objectsWithMorphTargets.length > 0 ) {
+        // Create Morph Folder
+        const morphFolder = pane.addFolder({
+            title: 'Morph Targets',
+          });
+
+        // Add a Blade
+        morphFolder.addBinding(currentObjectSelection, 'morphObject', {
+            options: objectOptions,
+          });
+
+        // Event Handler for Morph Pane
+        morphFolder.on( 'change', function( ev ) {
+            if( isNaN( ev.value ) === true ){
+                // Object List changed
+                currentObjectSelection.morphObject = ev.value;
+                
+                // Clear if it is NONE
+                if (ev.value === 'none'){
+                    for( let i = morphFolder.children.length-1; i > 0; i-- ) {
+                        morphFolder.children[i].dispose();
+                    }   
+                    return;
+                }
+                // Clear NameTargets
+                morphNameTargets = [];
+                // Feed the object of Morph Targets by getting the list of strings and associate the values
+                morphNameTargets = Object.keys( model.getObjectByName( ev.value ).morphTargetDictionary  ) ;
+                
+                // Reset UI from Last to second
+                if( morphFolder.children.length > 1 ){
+                    for( let i = morphFolder.children.length-1; i > 0; i-- ) {
+                        morphFolder.children[i].dispose();
+                    }    
+                }
+
+                // Create the UI
+                for( let i = 0; i < morphNameTargets.length; i++ ){
+                    morphFolder.addBlade({
+                        view: 'slider',
+                        label: morphNameTargets[i],
+                        min: 0,
+                        max: 1,
+                        value: model.getObjectByName( ev.value ).morphTargetInfluences[i],
+                    });
+                } 
+
+            } else {
+                // Sliders changed
+                model.getObjectByName(  currentObjectSelection.morphObject ).morphTargetInfluences[ morphNameTargets.indexOf( ev.target.label ) ] = ev.value ;
+            }
+          });
+    }
+}
+
+// Sockets ***************************************************
 
 // Behavior when receives CreateCamera msg
 socket.on( 'createCamera', function( msg ) {
@@ -172,12 +272,22 @@ socket.on( 'createCamera', function( msg ) {
 
 // Behavior when a user connects
 socket.on( 'userConnected', function( msg ) {
-    console.log( msg );
+    console.log( msg + " has connected " );
+    
+    // Add online users connected to the pane but not the current user
+    if( msg !== userName ) {
+        pane.addBlade({
+        view: 'text',
+        label: 'user',
+        parse: ( v ) => String( v ),
+        value: msg,
+        });
+    }
 });
 
 // Behavior when a user disconnects
 socket.on( 'userDisconnected', function( msg ) {
-    console.log( msg + ' has disconnected' );
+    console.log( msg + " has disconnected " );
     let tempCameraHelper = scene.getObjectByName( msg );
     if( tempCameraHelper !== null ){
         let tempCamera = tempCameraHelper.camera;
@@ -185,6 +295,10 @@ socket.on( 'userDisconnected', function( msg ) {
         tempCameraHelper.dispose();
         scene.remove( tempCamera );
     }
+
+    // Find the 'name' blade and remove it
+    let bladeDisposal = findBladeByLabel( pane, msg );
+    bladeDisposal.dispose();
 });
 
 socket.on( 'updateCamera', function( msg ){
@@ -203,6 +317,15 @@ socket.emit( 'createCamera', userName );
 
 // Check existing users and add their cameras - only happens one time
 socket.once( 'checkWhosOnline', function( msg ){
+    // Add current user to the pane
+    pane.addBlade({
+    view: 'text',
+    label: 'user (me)',
+    parse: ( v ) => String( v ),
+    value: userName,
+    });
+
+    // If there are more users online
     if( msg.length > 0 ){
         for( let k=0; k<msg.length; k++ ){
             let userCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -210,7 +333,20 @@ socket.once( 'checkWhosOnline', function( msg ){
             cameraHelper.name = msg[ k ];
             scene.add( userCamera );
             scene.add( cameraHelper );
+            
+            // Add online users connected to the pane
+            pane.addBlade({
+                view: 'text',
+                label: 'user',
+                parse: ( v ) => String( v ),
+                value: msg[ k ],
+              });
         }
         console.log('Added '+msg.length+' Cameras')
     }
+
+    // Initializate Scene
+    init();
+    animate();
+
 });
